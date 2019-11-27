bd9219fd8cdbbcd7ecdc613a66cc40967d75b90c
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-27 18:35:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   created_array = ["hello","mari","harmony","brooklyn"]
-  created_array << "nyc"
+  created_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
