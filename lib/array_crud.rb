8e5d64f7fbd79873ef7e45801548d16e4567b433
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-27 18:36:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   created_array = ["hello","mari","harmony","brooklyn"]
-  created_array.unshift("thanksgiving")
+  created_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
