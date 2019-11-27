9906b3d78d282f579e80044b863ff8ec85ccf4f2
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-27 18:33:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["hello","mari","harmony","brooklyn"]
-  
+  created_array = ["hello","mari","harmony","brooklyn"]
 end
 
 def add_element_to_end_of_array(array, element)
